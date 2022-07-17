2fc9bd5e44c5e8aaf2762364f16ef0a35963c674
Elliot Thomas <elliot@eth0net.io>
2022-07-17 10:12:32 +0100

    Tidy up EnchantMenu

--- src/main/kotlin/com/github/eth0net/enchantmenu/EnchantMenu.kt
+++ src/main/kotlin/com/github/eth0net/enchantmenu/EnchantMenu.kt
@@ -2,16 +2,11 @@ package com.github.eth0net.enchantmenu
 
 import com.github.eth0net.enchantmenu.screen.EnchantMenuScreenHandler
 import net.fabricmc.api.ModInitializer
-import net.fabricmc.fabric.api.networking.v1.PacketSender
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.entity.player.PlayerInventory
-import net.minecraft.network.PacketByteBuf
 import net.minecraft.screen.NamedScreenHandlerFactory
 import net.minecraft.screen.ScreenHandlerType
-import net.minecraft.server.MinecraftServer
-import net.minecraft.server.network.ServerPlayNetworkHandler
-import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.text.MutableText
 import net.minecraft.text.Text
 import net.minecraft.util.Identifier
@@ -27,19 +22,10 @@ object EnchantMenu : ModInitializer {
         Registry.register(Registry.SCREEN_HANDLER, id("enchant_menu"), ScreenHandlerType(::EnchantMenuScreenHandler))
     internal val OPEN_PACKET = id("open_enchant_menu")
 
-    object ScreenHandlerFactory : NamedScreenHandlerFactory {
-        override fun createMenu(syncId: Int, inv: PlayerInventory, player: PlayerEntity): EnchantMenuScreenHandler {
-            return EnchantMenuScreenHandler(syncId, inv)
-        }
-
-        override fun getDisplayName(): MutableText = Text.translatable("Enchant Menu")
-    }
-
-
     override fun onInitialize() {
         LOGGER.info("EnchantMenu initializing...")
 
-        ServerPlayNetworking.registerGlobalReceiver(OPEN_PACKET) { server, player, handler, buf, res ->
+        ServerPlayNetworking.registerGlobalReceiver(OPEN_PACKET) { _, player, _, _, _ ->
             player.openHandledScreen(ScreenHandlerFactory)
         }
 
@@ -47,4 +33,12 @@ object EnchantMenu : ModInitializer {
     }
 
     private fun id(path: String) = Identifier(MOD_ID, path)
+
+    object ScreenHandlerFactory : NamedScreenHandlerFactory {
+        override fun createMenu(syncId: Int, inv: PlayerInventory, player: PlayerEntity): EnchantMenuScreenHandler {
+            return EnchantMenuScreenHandler(syncId, inv)
+        }
+
+        override fun getDisplayName(): MutableText = Text.translatable("Enchant Menu")
+    }
 }
