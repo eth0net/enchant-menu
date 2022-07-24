a61ae68dcac6ebad85397be5576451343c1d09c9
Elliot Thomas <elliot@eth0net.io>
2022-07-24 13:31:37 +0100

    Update build files and bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,27 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-// import com.modrinth.minotaur.dependencies.ModDependency
-
 val javaVersion = JavaVersion.VERSION_17
 val minecraftVersion: String by project
 val yarnMappings: String by project
 val loaderVersion: String by project
 val fabricVersion: String by project
 val fabricKotlinVersion: String by project
-val modVersion: String by project
 val mavenGroup: String by project
 val modId: String by project
-val modReleaseType: String by project
-// val minotaurVersion: String by project
-// val modrinthToken = System.getenv("MODRINTH_TOKEN")
+val modVersion: String by project
+//val modVersionType: String by project
 
 plugins {
     id("fabric-loom")
     val kotlinVersion: String by System.getProperties()
     kotlin("jvm").version(kotlinVersion)
-    // id("com.modrinth.minotaur").version("2.+")
+//    id("com.modrinth.minotaur").version("2.+")
 }
 
 base { archivesName.set(modId) }
 
-version = modVersion
 group = mavenGroup
+version = modVersion
 
 repositories {}
 
@@ -61,21 +57,19 @@ tasks {
         withSourcesJar()
     }
 
-    // modrinth {
-    //     print("Uploading to Modrinth...")
-    //     token.set(modrinthToken)
-    //     projectId.set(archivesBaseName)
-    //     versionNumber.set(modVersion)
-    //     versionType.set(modReleaseType)
-    //     uploadFile.set(remapJar.get())
-    //     additionalFiles.set(mutableListOf(remapSourcesJar.get()))
-    //     gameVersions.add(minecraftVersion)
-    //     dependencies {
-    //         required.project("fabric-api")
-    //         required.project("fabric-language-kotlin")
-    //     }
-    //     syncBodyFrom.set(rootProject.file("README.md").toString())
-    //     debugMode.set(true)
-    //     print("Done!")
-    // }
+//    modrinth {
+//        print("Uploading to Modrinth...")
+//        debugMode.set(true)
+//        projectId.set(base.archivesName)
+//        versionType.set(modVersionType)
+//        uploadFile.set(remapJar.get())
+//        additionalFiles.set(listOf(remapSourcesJar.get()))
+//        gameVersions.add(minecraftVersion)
+//        dependencies {
+//            required.project("fabric-api")
+//            required.project("fabric-language-kotlin")
+//        }
+//        syncBodyFrom.set(rootProject.file("README.md").toString())
+//        print("Done!")
+//    }
 }
